a7f8b6887733fcb18bbae5389838f694aacb6ac5
unknown <canegallipedro@gmail.com>
2022-06-13 20:53:04 -0300

    Subiendo proyecto a Heroku

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const res = require('express/lib/response');
+const path = require('path');
+
+const app = express();
+
+app.listen(3000, function() {
+    console.log("Servidor corriendo")
+});
+
+const publicPath = path.resolve(__dirname, "./public");
+
+app.use(express.static(publicPath));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/home.html"))
+});
+
+app.get("/register", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/register.html"))
+});
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/login.html"))
+});
